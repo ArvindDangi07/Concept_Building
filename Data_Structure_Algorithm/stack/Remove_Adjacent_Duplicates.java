519ca51f3fa375da1f31d185c9f05ca3fc8869ba
ArvindDangi07 <arvindkumar131220@gmail.com>
2026-05-25 12:00:42 +0530

    Remove_Adjacent_Duplicates

---
+++ Data_Structure_Algorithm/stack/Remove_Adjacent_Duplicates.java
@@ -0,0 +1,26 @@
+package stack;
+
+import java.util.Stack;
+
+public class Remove_Adjacent_Duplicates {
+
+
+    public static void main(String[] args) {
+        String str = "abccdeef";
+
+        Stack<Character> stack = new Stack<Character>();
+
+        for (char c : str.toCharArray()) {
+            if(!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            }else{
+                stack.push(c);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(char c : stack){
+            sb.append(c);
+        }
+        System.out.println(sb.toString());
+    }
+}
